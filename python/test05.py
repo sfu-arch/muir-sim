a4073f451e645370f89c5595802edcfa712772b6
Amirali Sharifian <am.sharifian@gmail.com>
2020-04-04 12:17:36 -0700

    Update test05 python file

--- python/test05.py
+++ python/test05.py
@@ -22,6 +22,14 @@ a_s = dsim.DArray(val_a, dsim.DArray.DType.DWord)
 events = dsim.sim(ptrs = [a_s], vars= [], numRets=1, numEvents=1, hwlib = hw_lib_path)
 
 print("Cycle: " + str(events[0]))
-print("Output array:\n\t")
-print(a_s.getData())
-print(test05(val_a))
+
+if list(a_s.getData() == test05):
+    print("Sucessful Run!")
+    print("Output array:\t")
+    print(list(a_s.getData()))
+else:
+    print("Filed Run!")
+    print("Expected data:")
+    print(test05(val_a))
+    print("Return Data:")
+    print(list(a_s.getData()))
