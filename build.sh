9a326a1ba26c651c4af8abab9c988c14d32f0b38
Ali-Sed <asedagha@sfu.ca>
2021-04-01 19:33:15 -0700

    minor changes

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
+mkdir python/build
+
+nw=4
+ns=8
+tbe=8
+lock=8
+np=1
+
 rm -rf hardware/chisel/build
-python3 run.py --accel-config examples/memGenTest.json --nWays 4 --nSets 8 --tbe-depth 8 --lock-depth 8
+#for ns in 256, 512, 1024, 2048
+#do
+#    for nw in 4, 8, 16
+#    do
+        python3 run.py --accel-config examples/memGenTest.json --nWays $nw --nSets $ns --tbe-depth $tbe --lock-depth $lock --num-paral $np
+#done
+#done
+
+mv hardware/chisel/build/libhw.so python/build/libhw_$nw\_$ns\_$tbe\_$lock\_$np.so
