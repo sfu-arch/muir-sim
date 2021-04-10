cb5ec64d51411f292f345fb894d8456ee53b6272
Ali-Sed <asedagha@sfu.ca>
2021-04-10 01:58:48 -0700

    adding loop to build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,32 @@ mkdir python/build
 
 nw=4
 ns=8
-tbe=8
+tbe=32
 lock=8
 np=1
-nc=1
+nc=2
 nword=8
-rm -rf hardware/chisel/build
-#for ns in 256, 512, 1024, 2048
-#do
-#    for nw in 4, 8, 16
-#    do
-        python3 run.py --accel-config examples/memGenTest.json --nWays $nw --nSets $ns --tbe-depth $tbe --lock-depth $lock --num-paral $np --num-cache $nc --num-word $nword 
-#done
-#done
+for ns in 128 256 512
+do
+    for nw in 4 8
+    do
+	for np in 1 2 4 8
+	do
+	    for tbe in 8 16 32
+	    do
+                for nc in 1 2
+                do
+                   for nword in 8 16 32
+	           do
 
-mv hardware/chisel/build/libhw.so python/build/libhw_$nw\_$ns\_$tbe\_$lock\_$np\_$nc\_$nword.so
+	rm -rf hardware/chisel/build
+        python3 run.py --accel-config examples/memGenTest.json --nWays $nw --nSets $ns --tbe-depth $tbe --lock-depth $lock --num-paral $np --num-cache $nc --num-word $nword 
+	
+	mv hardware/chisel/build/libhw.so python/build/libhw_$nw\_$ns\_$tbe\_$lock\_$np\_$nc\_$nword.so
+done
+done
+done 
+done
+done
+done
+
