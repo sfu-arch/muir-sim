2d39545df1ab984079fe74a2001de93dff4f3587
miladhakimi <miladhakimi33@gmail.com>
2021-05-20 11:53:36 -0700

    Add compiler
    compiler.py added. It integrates event, transition, and state extractor

--- python/compiler/stateExtractor.py
+++ python/compiler/stateExtractor.py
@@ -1,42 +1,36 @@
 import re
-import sys
 
-location = "MI_example-cache.sm"
-if len(sys.argv) > 1:
-    location = sys.argv[1]
+def extract_states(file_name):
+    file = open(file_name, 'r')  
 
-file = open(location, 'r')
+    states = []
+    in_state = False
 
-in_state = False
-states = []
+    for line in file:
+        if "state_declaration" in line:
+            in_state = True
+        elif in_state:
+            if '}' in line:
+                break
+            
+            cleaned_arg = re.findall("[a-z|A-Z|_]+", line)
+            if not cleaned_arg:
+                continue
+            states.append(cleaned_arg[0])
 
-for line in file:
-    if "state_declaration" in line:
-        in_state = True
-    elif in_state:
-        if '}' in line:
-            break
-        
-        cleaned_arg = re.findall("[a-z|A-Z|_]+", line)
-        if not cleaned_arg:
-            continue
+    state_file = open("States.scala", "w")
 
-        print(cleaned_arg)
-        states.append(cleaned_arg[0])
+    state_file.write("package memGen.memory.cache\n\n\
+    import chisel3._\n\
+    import chisel3.util._\n\n\
+    object States{\n\n\
+    \tval StateArray = Map(\n")
 
-state_file = open("States.scala", "w")
+    text = '\t\t(s"{state}", {index})'
+    for index, state in enumerate(states):
+        state_file.write(text.format(state=state, index=hex(index)))
+        if state is not states[-1]:
+            state_file.write(', ')
+        state_file.write('\n')
 
-state_file.write("package memGen.memory.cache\n\n\
-import chisel3._\n\
-import chisel3.util._\n\n\
-object States{\n\n\
-\tval StateArray = Map(\n")
-
-text = '\t\t(s"{state}", {index})'
-for index, state in enumerate(states):
-    state_file.write(text.format(state=state, index=hex(index)))
-    if state is not states[-1]:
-        state_file.write(', ')
-    state_file.write('\n')
-
-state_file.write("\t)\n\tval stateLen =  if (StateArray.size == 1 ) 1 else log2Ceil(StateArray.size)\n}")
+    state_file.write("\t)\n\tval stateLen =  if (StateArray.size == 1 ) 1 else log2Ceil(StateArray.size)\n}")
